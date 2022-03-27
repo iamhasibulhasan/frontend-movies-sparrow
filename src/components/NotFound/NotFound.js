3ebca6ecd53ec251017ee4cba2e458141bdbdb30
imhasibulhasan <hashibul218@gmail.com>
2022-03-27 23:12:57 +0600

    404 page setup

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './NotFound.css';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>Not found page</h2>
+        <div className="section-404 padding-top padding-bottom">
+            <div className="container">
+                <div className="thumb-404">
+                    <img src="http://pixner.net/boleto/demo/assets/images/404.png" alt="404" />
+                </div>
+                <h3 className="title">Oops.. looks like you got lost :( </h3>
+                <Link to="/" className="custom-button">Back To Home <i className="flaticon-right"></i></Link>
+            </div>
         </div>
     );
 };
