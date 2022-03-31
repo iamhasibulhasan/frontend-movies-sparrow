08ced3d6ee663d7a18311f911cd70b73f9c90116
imhasibulhasan <hashibul218@gmail.com>
2022-03-31 12:33:25 +0600

    my-orders and manage-orders page setup

--- src/components/Auth/Dashboard/ManageOrders/ManageOrders.js
+++ src/components/Auth/Dashboard/ManageOrders/ManageOrders.js
@@ -1,10 +1,85 @@
 import React from 'react';
 import './ManageOrders.css';
+import { BsTrash, BsCheck2Circle } from "react-icons/bs";
+import { Badge } from 'react-bootstrap';
 
 const ManageOrders = () => {
     return (
-        <div>
-            <span>This is manage orders</span>
+        <div className='my-orders-sec'>
+            <h5 className='mb-4'>Manage All Orders</h5>
+            <table className='table table-dark table-hover'>
+                <thead>
+                    <tr>
+                        <td>#SI</td>
+                        <td>Customer Name</td>
+                        <td>Movie Name</td>
+                        <td>Date</td>
+                        <td>Status</td>
+                        <td>Action</td>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Hasibul Hasan</td>
+                        <td>Harry Potter</td>
+                        <td>12/02/2023</td>
+                        <td>
+                            <Badge bg="success">Completed..</Badge>
+                        </td>
+                        <td className='d-flex gap-2'>
+                            <a href="#0" className='btn btn-success disabled' ><BsCheck2Circle /></a>
+                            <a href="#0" className='btn btn-danger'><BsTrash /></a>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>2</td>
+                        <td>Hasibul Hasan</td>
+                        <td>Harry Potter</td>
+                        <td>12/02/2023</td>
+                        <td>
+                            <Badge bg="danger">Pending..</Badge>
+                        </td>
+                        <td className='d-flex gap-2'>
+                            <a href="#0" className='btn btn-success'><BsCheck2Circle /></a>
+                            <a href="#0" className='btn btn-danger'><BsTrash /></a>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>3</td>
+                        <td>Hasibul Hasan</td>
+                        <td>Harry Potter</td>
+                        <td>12/02/2023</td>
+                        <td>
+                            <Badge bg="danger">Pending..</Badge>
+                        </td>
+                        <td className='d-flex gap-2'>
+                            <a href="#0" className='btn btn-success'><BsCheck2Circle /></a>
+                            <a href="#0" className='btn btn-danger'><BsTrash /></a>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>4</td>
+                        <td>Hasibul Hasan</td>
+                        <td>Harry Potter</td>
+                        <td>12/02/2023</td>
+                        <td>
+                            <Badge bg="danger">Pending..</Badge>
+                        </td>
+                        <td className='d-flex gap-2'>
+                            <a href="#0" className='btn btn-success'><BsCheck2Circle /></a>
+                            <a href="#0" className='btn btn-danger'><BsTrash /></a>
+                        </td>
+                    </tr>
+
+
+
+                </tbody>
+            </table>
         </div>
     );
 };
--- src/components/Auth/Dashboard/MyOrders/MyOrders.js
+++ src/components/Auth/Dashboard/MyOrders/MyOrders.js
@@ -1,10 +1,87 @@
 import React from 'react';
+import { Badge } from 'react-bootstrap';
 import './MyOrders.css';
+import { BsTrash } from "react-icons/bs";
 
 const MyOrders = () => {
     return (
-        <div>
-            <span>This my orders</span>
+        <div className='my-orders-sec'>
+            <h5 className='mb-4'>My Orders</h5>
+            <table className='table table-dark table-hover'>
+                <thead>
+                    <tr>
+                        <td>#SI</td>
+                        <td>Movie Name</td>
+                        <td>On Screen</td>
+                        <td>Date</td>
+                        <td>Hall Name</td>
+                        <td>Ticket Price</td>
+                        <td>Status</td>
+                        <td>Action</td>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Harry Potter</td>
+                        <td>13:25</td>
+                        <td>12/02/2023</td>
+                        <td>Balaka</td>
+                        <td>$260</td>
+                        <td>
+                            <Badge bg="danger">Pending..</Badge>
+                        </td>
+                        <td>
+                            <a href="#0" className='btn btn-danger'><BsTrash /></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Harry Potter</td>
+                        <td>13:25</td>
+                        <td>12/02/2023</td>
+                        <td>Balaka</td>
+                        <td>$260</td>
+                        <td>
+                            <Badge bg="success">Completed..</Badge>
+                        </td>
+                        <td>
+                            <a href="#0" className='btn btn-danger'><BsTrash /></a>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>3</td>
+                        <td>Harry Potter</td>
+                        <td>13:25</td>
+                        <td>12/02/2023</td>
+                        <td>Balaka</td>
+                        <td>$260</td>
+                        <td>
+                            <Badge bg="danger">Pending..</Badge>
+                        </td>
+                        <td>
+                            <a href="#0" className='btn btn-danger'><BsTrash /></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>Harry Potter</td>
+                        <td>13:25</td>
+                        <td>12/02/2023</td>
+                        <td>Balaka</td>
+                        <td>$260</td>
+                        <td>
+                            <Badge bg="danger">Pending..</Badge>
+                        </td>
+                        <td>
+                            <a href="#0" className='btn btn-danger'><BsTrash /></a>
+                        </td>
+                    </tr>
+
+                </tbody>
+            </table>
         </div>
     );
 };
