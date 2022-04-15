52c3db49d90f0fc4b3ed86655eca0eef58ce3a71
imhasibulhasan <hashibul218@gmail.com>
2022-04-15 11:55:32 +0600

    edit movie data

--- src/components/Auth/Dashboard/AllMovies/AllMovies.js
+++ src/components/Auth/Dashboard/AllMovies/AllMovies.js
@@ -5,71 +5,87 @@ import useFunction from './../../../../hooks/useFunction';
 import { Link } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import DataTable, { createTheme } from 'react-data-table-component';
+import { Button, Modal } from 'react-bootstrap';
 
 const AllMovies = () => {
     const { http } = useFunction();
     const [movies, setMovies] = useState([]);
+    const [show, setShow] = useState(false);
+    const [editData, setEditData] = useState([]);
 
     useEffect(() => {
         http.get('./movies')
             .then(res => {
                 setMovies(res.data.movies);
             })
-    }, []);
-
-
-    // Data table
-    const columns = [
-        {
-            name: 'Movie Name',
-            selector: row => row.movieName,
-        },
-        {
-            name: 'Type',
-            selector: row => row.category,
-        },
-        {
-            name: 'Ticket Price',
-            selector: row => '$ ' + row.ticketPrice,
-        },
-        {
-            name: 'Action',
-            selector: row => <span><a href="#0" className='btn btn-warning btn-sm'><BsPencilSquare /></a>  <Link to='' onClick={(e) => handleDelete(e, row._id)} className='btn btn-danger btn-sm'><BsTrash /></Link></span>,
-
-
-        }
-    ];
-
-
-    createTheme('solarized', {
-        text: {
-            primary: '#268bd2',
-            secondary: '#2aa198',
-        },
-        background: {
-            default: '#032055',
-        },
-        context: {
-            background: '#cb4b16',
-            text: '#FFFFFF',
-        },
-        divider: {
-            default: '#073642',
-        },
-        action: {
-            button: 'rgba(0,0,0,.54)',
-            hover: 'rgba(0,0,0,.08)',
-            disabled: 'rgba(0,0,0,.12)',
-        },
-    }, 'dark');
+    }, [movies]);
 
 
 
 
+    const handleCloseModal = () => setShow(false);
+
+    const handleShowModal = (e, id) => {
+        e.preventDefault();
+
+        movies.filter(m => m._id === id).map(edit => {
+
+            setEditData(edit);
+        })
 
 
-    // Data table ends
+        setShow(true);
+    };
 
+    const handleChangeName = (e) => {
+        setEditData({ _id: editData._id, movieName: e.target.value, description: editData.description, ticketPrice: editData.ticketPrice, category: editData.category });
+
+
+    }
+    const handleChangeDes = (e) => {
+        setEditData({ _id: editData._id, movieName: editData.movieName, description: e.target.value, ticketPrice: editData.ticketPrice, category: editData.category });
+
+    }
+    const handleChangePrice = (e) => {
+
+        setEditData({ _id: editData._id, movieName: editData.movieName, description: editData.description, ticketPrice: e.target.value, category: editData.category });
+    }
+    const handleChangeCat = (e) => {
+
+        setEditData({ _id: editData._id, movieName: editData.movieName, description: editData.description, ticketPrice: editData.ticketPrice, category: e.target.value });
+    }
+
+    // Movie Update route
+    const handleEdit = (e) => {
+        http.patch('/movie', {
+            ...editData
+        }).then(res => {
+            // console.log(res.data.modifiedCount);
+            if (res.data.modifiedCount) {
+                Swal.fire({
+                    toast: true,
+                    position: 'top-end',
+                    icon: 'success',
+                    title: 'Movie data modified',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+            } else {
+                Swal.fire({
+                    toast: true,
+                    position: 'top-end',
+                    icon: 'error',
+                    title: 'Something is wrong',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+            }
+        })
+
+        handleCloseModal();
+        e.preventDefault();
+
+    }
     // Movie Delete route
     const handleDelete = (e, id) => {
         e.preventDefault();
@@ -121,6 +137,54 @@ const AllMovies = () => {
 
     }
 
+    // Data table
+    const columns = [
+        {
+            name: 'Movie Name',
+            selector: row => row.movieName,
+        },
+        {
+            name: 'Type',
+            selector: row => row.category,
+        },
+        {
+            name: 'Ticket Price',
+            selector: row => '$ ' + row.ticketPrice,
+        },
+        {
+            name: 'Action',
+            selector: row => <span><Link to='' onClick={(e) => handleShowModal(e, row._id)} className='btn btn-warning btn-sm'><BsPencilSquare /></Link>  <Link to='' onClick={(e) => handleDelete(e, row._id)} className='btn btn-danger btn-sm'><BsTrash /></Link></span>,
+
+
+        }
+    ];
+
+
+    createTheme('solarized', {
+        text: {
+            primary: '#268bd2',
+            secondary: '#2aa198',
+        },
+        background: {
+            default: '#032055',
+        },
+        context: {
+            background: '#cb4b16',
+            text: '#FFFFFF',
+        },
+        divider: {
+            default: '#073642',
+        },
+        action: {
+            button: 'rgba(0,0,0,.54)',
+            hover: 'rgba(0,0,0,.08)',
+            disabled: 'rgba(0,0,0,.12)',
+        },
+    }, 'dark');
+
+    // Data table ends
+
+
 
 
     return (
@@ -141,6 +205,42 @@ const AllMovies = () => {
                     responsive
                 />
             </div>
+            <Modal show={show} onHide={handleCloseModal} className='text-black'>
+                <form onSubmit={handleEdit}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Edit Movies</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <input value={editData._id} type='hidden' />
+
+                        <label htmlFor="">Movie Name</label>
+                        <input onChange={handleChangeName} value={editData.movieName} className='form-control mb-4' placeholder='Movie name' />
+
+                        <label htmlFor="">Description</label>
+                        <textarea onChange={handleChangeDes} value={editData.description} className='form-control mb-4' placeholder='Description' />
+
+                        <label htmlFor="">Ticket Price</label>
+                        <input onChange={handleChangePrice} value={editData.ticketPrice} className='form-control mb-4' placeholder='Ticket price' />
+
+                        <label htmlFor="">Category</label>
+                        <input onChange={handleChangeCat} value={editData.category} className='form-control mb-4' placeholder='Category' />
+
+
+
+
+
+
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleCloseModal}>
+                            Close
+                        </Button>
+                        <Button type='submit' variant="primary" onClick={handleCloseModal}>
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
+                </form>
+            </Modal>
         </div>
     );
 };
